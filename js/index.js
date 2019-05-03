891bcba594c50569e4510be5d6cebc905a9b43e6
ersoper1 <48351311+ersoper1@users.noreply.github.com>
2019-05-03 12:31:28 +0100

    Update index.js
    img swap function on team id change

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function imageSwap() {
+    if (teamID.value == "CMDblues") {
+        document.getElementById("teamBadge").src="graphics/CMDblues.png";
+    }
+    else if (teamID.value == "CMDreds") {
+        document.getElementById("teamBadge").src="graphics/CMDreds.png";
+    }
+}
